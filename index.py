6ac8a3f488de468a3c6ecc697341a46b23da1caf
Josh Kralewski <joshkmanj@gmail.com>
2024-04-03 14:23:09 -0500

    Added notes about python specific syntax

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 # Imports from openpyxl
+# *** In python, imports are declared with the structure "from <module> import <class>"
 from openpyxl import load_workbook, Workbook
 
 # Define the paths
+# *** In python, variables are dynamically typed and do not require a "const" or "let" keyword
 existing_file_path = 'data/input/test_input.xlsx'
 new_file_path = 'data/output/test_output.xlsx'
 
@@ -13,6 +15,7 @@ ws = wb.active
 # ----------< Tasks begin >------------
 
 # If the first column of the first row starts with "Payout Report", copy the first row
+# *** In python, if statements are declared without parentheses and with a colon at the end, code block to be executed is indented instead of within brackets
 if ws['A1'].value.startswith("Payout Report"):
     # Copy the text from the first column of the first row
     payout_report = ws['A1'].value
@@ -24,4 +27,5 @@ if ws['A1'].value.startswith("Payout Report"):
 # Save the workbook to a new file
 wb.save(new_file_path)
 
+# *** In python, instead of console logs, use "print(<message>)"
 print("Workbook saved to", new_file_path)
