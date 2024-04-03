07cf4acbbf841a79eed74df7b8bd7a5064d4ae1d
Josh Kralewski <joshkmanj@gmail.com>
2024-04-03 14:59:08 -0500

    Cleaned up excess notes, organized for readability

--- index.py
+++ index.py
@@ -5,36 +5,36 @@ from openpyxl import load_workbook, Workbook # *** In python, imports are declar
 existing_file_path = 'data/input/test_input.xlsx' # *** In python, variables are dynamically typed and do not require a "const" or "let" keyword
 new_file_path = 'data/output/test_output.xlsx'
 
-# load workbook & get active worksheet
+# Load worksheet
 wb = load_workbook(filename=existing_file_path)
 ws = wb.active
 
-
-# ----------< Tasks begin >------------
-# Initialize variables
+# --------------------<  T a s k s  b e g i n  >----------------------
+# Variables
 spreadsheet_title = None 
 
-# 1. Get title
+# 1. Get title and remove from first row
 if ws['A1'].value.startswith("Payout Report"): # *** In python, if statements are declared without parentheses and with a colon at the end, code block to be executed is indented instead of within brackets
-    # Copy the text from the first column of the first row
     spreadsheet_title = ws['A1'].value
 else:
     print("Error: Spreadsheet title not found")
     # add other error handling code here
 print("Payout Report:", spreadsheet_title)
 
-# 2. Cut the first row
 ws.delete_rows(1)
 
-# 3. Add two empty rows to the end of the worksheet
-ws.append([])
-ws.append([])
+# 2. 
 
-# 4. Append spreadsheet title to a new row at the bottom of the worksheet
+
+
+
+
+# Last. Append title to the end of sheet with buffer space
+ws.append([])
+ws.append([])
 ws.append([spreadsheet_title])
 
-
-# ----------< // Tasks End >------------
+# --------------------< //  T a s k s  E n d  >----------------------
 
 # Save the workbook to a new file
 wb.save(new_file_path)
